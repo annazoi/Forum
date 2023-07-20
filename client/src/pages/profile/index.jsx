f8ee3dd9cd858b2902ca908f7001f617060034cd
annazoi <zoianna01@gmail.com>
2023-07-20 15:48:28 +0300

    design

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -4,52 +4,55 @@ import { useProfileHook } from "../../hooks/profileHook";
 import Button from "../../components/ui/Button";
 import { useImageHook } from "../../hooks/imageHook";
 import { usePostHook } from "../../hooks/postHook";
-
-// const Profile = () => {
-//   const [user, setUser] = useState({});
-//   const { getProfile } = useProfileHook();
-//   const { getImage } = useImageHook();
-
-//   useEffect(() => {
-//     const getSpecificProfile = async () => {
-//       try {
-//         const response = await getProfile();
-//         setUser(response.data);
-//         console.log(response.data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     getSpecificProfile();
-//   }, []);
-
-//   return (
-//     <>
-//       <div>
-//         <div className="user-content">
-//           <h1>Username: {user.username}</h1>
-//           <h1>name: {user.name}</h1>
-//           <h1>surname: {user.surname}</h1>
-//           <h1>Email: {user.email}</h1>
-//         </div>
-//         <Button label="Settings"></Button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Profile;
+import { toast } from "react-toastify";
 
 const Profile = () => {
-  const { getPost } = usePostHook();
+  const [user, setUser] = useState({});
+  const { getProfile } = useProfileHook();
+  const { getImage } = useImageHook();
+  const [image, setImage] = useState("");
 
   useEffect(() => {
-    const post = async () => {
-      const response = await getPost();
-      console.log(response.data);
+    const getSpecificProfile = async () => {
+      try {
+        const response = await getProfile();
+        setUser(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+
+    getSpecificProfile();
   }, []);
+
+  const handleImage = (event) => {
+    const file = event.target.files[0];
+    setFileToBase(file);
+    console.log(file);
+  };
+
+  const setFileToBase = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+  };
+
+  return (
+    <>
+      <div>
+        <div className="user-content">
+          <h1>Username: {user.username}</h1>
+          <h1>name: {user.name}</h1>
+          <h1>surname: {user.surname}</h1>
+          <h1>Email: {user.email}</h1>
+        </div>
+        <Button label="Settings"></Button>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
